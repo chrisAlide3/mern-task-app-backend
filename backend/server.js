aacb4cb7f0b9751153b1d0254c02ee0d28f737be
chrisAlide3 <christian.bracher@gmail.com>
2023-06-15 09:34:16 +0200

    Updated Cors Policy

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://mern-task-app-jjjt.onrender.com/",
+      "https://mern-task-app-jjjt.onrender.com",
     ],
   })
 );
